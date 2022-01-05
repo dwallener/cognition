5f491716b6251b9f67a4aef25e6b300562137de6
Damir Wallener <damir00@damirMBP.local>
2022-01-05 11:50:35 -0800

    added guassian blurring of image list

--- memory-degradation/memory-degradation.py
+++ memory-degradation/memory-degradation.py
@@ -18,8 +18,15 @@
 # With each refresh in a time interval Tint < Threshold_refresh, bump the detect sensitivity by +5.
 # With each lack of refresh in a time interval > Threshold_Remember, decrease the sensitivity by -5.
 
-from PIL import Image
+import logging
+logging.basicConfig(filename='memory-degradation.log', encoding='utf-8', level=logging.DEBUG)
+logging.warning("REBOOT HAPPENED HERE ===============================================")
+
+from PIL import Image, ImageFilter
 import glob
+
+import numpy
+# import cv2 let's try and avoid this dependency
 
 # Load an array of images
 
@@ -27,8 +34,16 @@ image_list = []
 for filename in glob.glob('../datasets/tufts_td_cs/*./jpg'):
     im = Image.open(filename)
     image_list.append(im)
-
+logging.info("Imported a directory of images")
 
 # perform guassian blur on each image
+blurred_image_list = []
+for i in image_list:
+    # do the blur - opencv
+    # dst = cv2.GaussianBlur(i, (5,5), cv2.BORDER_DEFAULT)
+    i2 = i.filter(ImageFilter.GaussianBlur(radius=2))
+    logging.info("Blurred an image")
+    blurred_image_list.append(i2)
+logging.info("Blurred a list of images")
 
 # perform edge detect on each image
